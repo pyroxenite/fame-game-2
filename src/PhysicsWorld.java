c5ae932ff543eda2a0650ad6a018226d32f77c7a
Pharoah Jardin <pharoah8060@gmail.com>
2021-05-20 00:24:08 +0200

    Physics

---
+++ src/PhysicsWorld.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+
+public class PhysicsWorld {
+  private ArrayList<Rectangle> rects = new ArrayList<>();
+
+  public PhysicsWorld() {
+
+  }
+  
+  public PhysicsWorld add(Rectangle rect) {
+    this.rects.add(rect);
+    return this;
+  }
+  
+  public void draw() {
+    this.rects.forEach(r -> r.draw());
+  }
+  
+  public void update() {
+    for (var i=0; i<rects.size(); i++) {
+      Rectangle r = rects.get(i);
+      if (!r.isFixed() && i != 0) {
+        r.getNextVel().setY(r.getVel().getY() + 0.4); // gravity
+        r.getNextVel().scale(0.98); // friction
+        r.getNextPos().add(r.getVel());
+      }
+    }
+    
+    for (var i=0; i<10; i++) {
+      rects.forEach(r1 -> {
+        rects.forEach(r2 -> {
+          if (r1 != r2)
+            seperate(r1, r2);
+        });
+      });
+    }
+    
+    rects.forEach(r -> r.nextFrame());
+  }
+  
+  private void seperate(Rectangle r1, Rectangle r2) {
+    double left =  r2.getPos().getX() - r2.getWidth()/2 - r1.getPos().getX() - r1.getWidth()/2;
+    double right = r1.getPos().getX() - r1.getWidth()/2 - r2.getPos().getX() - r2.getWidth()/2;
+    double top =  r2.getPos().getY() - r2.getHeight()/2 - r1.getPos().getY() - r1.getHeight()/2;
+    double bottom = r1.getPos().getY() - r1.getHeight()/2 - r2.getPos().getY() - r2.getHeight()/2;
+    
+    boolean overlapping = (top < 0) && (right < 0) && (bottom < 0) && (left < 0);
+    
+    if (overlapping && !r1.isFixed()) {
+      double condition = r1.isFixed()?1:0;
+      if (left > right && left > top && left > bottom) {
+        r1.getNextPos().setX(r1.getPos().getX() + left/2 + condition*left/2);
+        r1.getNextVel().setX(-(r1.getVel().getX()-r2.getVel().getX())*0.2 + r2.getVel().getX());
+      } else if (right > top && right > bottom) {
+        r1.getNextPos().setX(r1.getPos().getX() - right/2 - condition*right/2);
+        r1.getNextVel().setX(-(r1.getVel().getX()-r2.getVel().getX())*0.2 - r2.getVel().getX()*0.6);
+      } else if (top > bottom) {
+        r1.getNextPos().setY(r1.getPos().getY() + top/2 + condition*top/2 - 0.01);
+        r1.getNextVel().setY(-(r2.getVel().getY()-r1.getVel().getY())*0.4);
+      } else {
+        r1.getNextPos().setY(r1.getPos().getY() - bottom/2 - condition*bottom/2 + 0.01);
+        r1.getNextVel().setY(-(r2.getVel().getY()-r1.getVel().getY())*0.4);
+      }
+    }
+  }
+}
---
+++ src/Rectangle.java
@@ -0,0 +1,81 @@
+public class Rectangle {
+    private Vector pos;
+    private Vector vel;
+    private Vector nextPos;
+    private Vector nextVel;
+    private double w;
+    private double h;
+    private boolean isFixed = false;
+
+    public Rectangle(double x, double y, double w, double h) {
+        pos = new Vector(x, y);
+        nextPos = new Vector(x, y);
+        vel = new Vector(0, 0);
+        nextVel = new Vector(0, 0);
+        this.w = w;
+        this.h = h;
+    }
+
+    public Vector getPos() {
+        return pos;
+    }
+
+    public void setPos(Vector pos) {
+        this.pos = pos;
+    }
+
+    public Vector getVel() {
+        return vel;
+    }
+
+    public void setVel(Vector vel) {
+        this.vel = vel;
+    }
+
+    public Vector getNextPos() {
+        return nextPos;
+    }
+
+    public void setNextPos(Vector nextPos) {
+        this.nextPos = nextPos;
+    }
+
+    public Vector getNextVel() {
+        return nextVel;
+    }
+
+    public void setNextVel(Vector nextVel) {
+        this.nextVel = nextVel;
+    }
+
+    public boolean isFixed() {
+        return isFixed;
+    }
+
+    public Rectangle setFixed() {
+        isFixed = true;
+        return this;
+    }
+
+    public double getWidth() {
+        return w;
+    }
+
+    public double getHeight() {
+        return h;
+    }
+
+    public void draw() {
+        some_rect_drawing_function(
+            this.pos.getX() - this.w / 2, 
+            this.pos.getY() - this.h / 2, 
+            this.w, 
+            this.h
+        );
+    }
+
+    public void nextFrame() {
+        this.pos = this.nextPos;
+        this.vel = this.nextVel;
+    }
+}
